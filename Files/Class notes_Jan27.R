4c9cf1b1af9019d5782c8bd9fcce71fdb49c710f
Lauren Upadhyay <lauren@nbp-2-107.nbp.ruw.rutgers.edu>
2016-01-27 15:35:33 -0500

    Final notes for the day 1/27

--- Files/Class notes_Jan27.R
+++ Files/Class notes_Jan27.R
@@ -32,10 +32,68 @@ z2
                 #most packages assume data will be in a data frame
 
 
+
 #Array=multiply subscripted collection of data entries
         #dimension vector is vector of non-neg integers. If length=k,
             #vector has k dimensions
 #Matrix=two-dimensional array
 matrix(data_vector, nrow = , ncol = ) #used to specify a matrix format for a vector
 z = matrix(h, nrow=5, ncol=2) #will create (5x2) matrix z
+
+#below code taken from matrix.R file
+rm(list=ls())
+h = c(1,3,5,7,9,12,14,16,18,20)
+h
+z = matrix(h,nrow=5,ncol=2)
+z
+w = matrix(h,nrow=6,ncol=3)
+w
+v = matrix(z,20,2)
+v
+
+
+
+#Lists
+Empl = list (employee="Anna", spouse = "Fred",
+             children = 3, child.ages=c(4,7,9))
+#name$component_name more convenient
+#names$child.ages[3] refers to third element in child ages 
+                                #(without bracket would refer to all)
+
+#combine function c can combine lists or add new components to list
+
+#below code is taken from Examples+of+List.R file
+rm(list=ls())
+
+Empl <- list( employee=c("Anna", "Bob", "Sarah"),spouse=c("Fred", "Anna", "John"), 
+              children=c(3,2,0), child.ages = c( list(c(4,7,9)), list(c(5,8)), list(NA) ) )
+
+Empl
+
+Empl = c(Empl, list(service=c(8,10,7))) #adds new component to existing list
+
+Empl
+
+
+#Data Frames
+#used to store a data matrix
+#should be thought of as a list of variables of same length, 
+                                     #but possibly different types
+
+#below code is take from read+data+using+read_table.R file
+rm(list=ls())
+
+# specify a vector of names 
+
+names = c("NO2_conc","cars","temp","wind","temp_diff","wind_dir","hour","day")
+
+no2_1 <- read.table("NO2.dat",header=FALSE,col.names=names,row.names=NULL) 
+
+city <- rep("Oslo",500)  # Note the use of the rep command
+
+no2_2 <- data.frame(no2_1, city)
+no2_2[1:10,]
+
+#(I do not have the NO2 data so this code will not run, 
+                                # but good for example purposes)
 
