d28df4f249aa5d9f25f57efe6da2c50e9bb5928c
Lauren Upadhyay <lauren@nbp-2-228.nbp.ruw.rutgers.edu>
2016-01-27 16:31:38 -0500

    Experiment changed from cards to coin. Histogram image created.

--- Files/Homework1.R
+++ Files/Homework1.R
@@ -1,15 +1,14 @@
 #Analytical Methods Homework 1
 
-#Probability of drawing a red or black card from a deck
-#black = success, red = failure
+#Probability of flipping heads wth a coin
 
     #probability of success
-n = 5        #number of trials
+n = 10        #number of trials
 m = 20       #number of experiments
 
 
-x<-c(1,4,3,2,5,3,2,1,0,3,4,4,2,2,0,0,1,5,3,2) #number of successes per trial
+x<-c(6,2,7,4,10,5,6,1,5,3,4,8,2,2,0,9,3,5,3,2) #number of successes per trial
 x
 
-hist(x,main="20 Trials of Drawing Cards from a Deck",
-     ylab="Frequency",xlab="Number of Successes (Red Card Drawn)")
+hist(x,main="20 Trials of Flipping a Coin",
+     ylab="Frequency",xlab="Number of Successes (Heads)")
