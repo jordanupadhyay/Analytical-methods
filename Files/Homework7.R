e2908b809c66dd5c17d495f9a4d98b24dd4796e5
Lauren Upadhyay <lauren@Laurens-MacBook-Pro.local>
2016-04-03 23:45:07 -0400

    Finished stratified random sampling problem

--- Files/Homework7.R
+++ Files/Homework7.R
@@ -127,27 +127,50 @@ strat1 = c(97, 67, 42, 125, 25, 92, 105, 86, 27, 43, 45, 59, 53, 21)
 N1 = 80
 n1 = 14
 mean1 = mean(strat1)
+var1 = var(strat1)
 mean1
+var1
 
 strat2 = c(155, 67, 96, 256, 47, 310, 236, 220, 352, 142, 190)
 N2 = 70
 n2 = 12
 mean2 = mean(strat2)
+var2 = var(strat2)
 mean2
+var2
 
 strat3 = c(142, 256, 310, 440, 495, 510, 320, 396, 196)
 N3 = 50
 n3 =9
 mean3 = mean(strat3)
+var3 = var(strat3)
 mean3
+var3
 
 strat4 = c(167,655,220,540,780)
 N4 = 30
 n4 = 5
 mean4 = mean(strat4)
+var4 = var(strat4)
 mean4
+var4
 
 y_bar = ((N1*mean1)+(N2*mean2)+(N3*mean3)+(N4*mean4))/230
+var_st = (1/230^2)*(((N1*(N1-n1)*var1)/n1)+((N2*(N2-n2)*var2)/n2)+
+                  ((N3*(N3-n3)*var3)/n3)+((N1*(N4-n4)*var4)/n4))
 y_bar
+var_st
 
+s= sqrt(var_st)
+s
+ci = 2*s
+ci
 
+t = 230*y_bar
+s2_t = (230^2)*var_st
+s_t = sqrt(s2_t)
+ci_t = 2*s_t
+
+t
+s2_t
+ci_t
