ee228522ddf3556bdb353cb0737326269bc3a889
Lauren Upadhyay <lauren@Laurens-MacBook-Pro.local>
2016-05-01 19:16:02 -0400

    Recalculated VMR

---
+++ Untitled.R
@@ -0,0 +1,14 @@
+#x = rbinom(sim_size, sam_size, p)
+par(mfrow=c(3,2))
+
+x1 = rbinom(10, 10, 0.5)
+x2 = rbinom(1000, 10, 0.5)
+
+hist(x1, main="10 simulations, n = 10, p = 0.5")
+hist(x2. main="1000 simulations, n = 10, p = 0.5")
+
+y1 = rbinom(10, 1000, 0.5)
+y2 = rbinom(1000, 1000, 0.5)
+
+z1 = rbinom(10, 1000, 0.2)
+z2 = rbinom(1000, 1000, 0.2)
