d27648e2c2fed6c80f29088f2c5ab86d2036d3fb
Lauren Upadhyay <lauren@Laurens-MacBook-Pro.local>
2016-05-01 19:30:08 -0400

    Completed question 4

--- Homework9.R
+++ Homework9.R
@@ -1,9 +1,15 @@
+#Question 4
 h = c(11,5,7,16,6,8,11,12,6,12,16,15,9,14,9)
 data = data.frame(h)
 data
 k = 15
-std = sqrt(2/14)   #[1] 0.3779645
-s_vmr = var(h)      #[1] 13.69524
-xbar = mean(h)   #[1] 10.46667
+s_vmr = sqrt(2/14) #[1] 0.3779645
+s2 = var(h)        #[1] 13.69524
+xbar = mean(h)     #[1] 10.46667
 
-VMR = s_vmr/xbar #[1] 1.308462
+VMR = s_vmr/xbar   #[1] 1.308462
+
+t = (VMR-1)/s_vmr  #[1] -2.55021
+qt(0.975,14)
+
+#Question 5
